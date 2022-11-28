e616ade6180b7f571d4ca27cf7f53e0f7d16ed2f
Omar Baldi <omarbaldidk@gmail.com>
2022-11-28 17:45:59 +0100

    Removed unecessary files and written down first description to what the application is meant to do

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+/**
+ *
+ * @desc As soon as the user clicks anywhere in the window width
+ * I want to retrieve the coordinates (x and y) and place a marker
+ * in the exact same position clicked.
+ *
+ * The user will also be able to UNDO/REDO the operations made.
+ *
+ * If the UNDO button is clicked, the last dot will be removed
+ * from the window view. The button will be disabled if there
+ * are no more dots to remove.
+ *
+ * If the REDO button is instead clicked, all of the dots previously
+ * removed (following point above) will be drawn again in the exact
+ * same position as before. The button will be disabled if there
+ * are no more removed dots to insert back again.
+ */
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <div className='App'></div>;
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const elementRoot = document.getElementById('root') as HTMLElement;
+
+ReactDOM.createRoot(elementRoot).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
